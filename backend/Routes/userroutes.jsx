aebc977cb2f41d2de7b0f67c4dae55aac1b76b80
Srinivas <vasus4990@gmail.com>
2024-03-16 20:38:14 +0530

    user routes updated

--- backend/Routes/userroutes.jsx
+++ backend/Routes/userroutes.jsx
@@ -8,6 +8,6 @@ response.get("/view/:email", users.viewbyemail);
 response.delete("/delete/:id", users.deleteusers);
 response.put("/Credit/", users.Credit);
 response.put("/Withdrawl/", users.Withdrawl);
-response.put("/editusers/:id/", users.editusers);
+
 
 module.exports = response;
